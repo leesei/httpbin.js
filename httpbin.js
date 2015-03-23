54905e28bedba46942a559bf64997327853f8bc0
leesei <leesei@gmail.com>
2015-03-23 18:00:30 +0800

    use Hapi, update attributes to better match HTTPBin
     Changes to be committed:
    	modified:   LICENSE
    	modified:   httpbin.js
    	modified:   package.json

--- httpbin.js
+++ httpbin.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var bunyan = require('bunyan');
+var Bunyan = require('bunyan');
+var Hapi = require('hapi');
+var Url = require('url');
 
 var APP_NAME = "httpbin.js";
 
@@ -16,40 +15,78 @@ var argv = require("nomnom")
   })
   .option('body', {
     abbr: 'b',
-    help: "put reqest attributes as reponse body",
+    help: "put request attributes as reponse body",
     flag: true,
-    default: false
+    default: true
   })
   .parse();
 // console.log(argv);
 
-var log = bunyan.createLogger({name: APP_NAME});
+var log = Bunyan.createLogger({name: APP_NAME});
+
+var server = new Hapi.Server();
+server.connection({
+  port: argv.port,
+  routes: {
+    cors: true
+  }
+});
+
+server.route({
+  method: '*',
+  path: '/{path*}',
+  handler: function (request, reply) {
+    // these are the interested attributes
+    var attrs = {
+      headers: request.headers,
+      hostname: request.info.hostname,
+      origin: request.info.remoteAddress,
+      url: 'http://' + request.headers.host + request.raw.req.url,
+      path: request.path,
+      query: request.query,
+      body: request.payload
+    };
+    log.info(attrs);
+    // log.info(request.raw.req);
+    if (argv.body) {
+      return reply(attrs);
+    }
+    else {
+      return reply();
+    }
+  }
+});
+
+
+server.start(function() {
+  console.log('Server running at:', server.info.uri);
+});
 
 // http://expressjs.com/4x/api.html#request
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(function (req, res) {
-  // these are the interested attributes
-  var attrs = {
-    headers: req.headers,
-    hostname: req.hostname,
-    originalUrl: req.originalUrl,
-    path: req.path,
-    query: req.query,
-    body: req.body,
-  };
-  log.info(attrs);
-  if (argv.body) {
-    res.status(200).json(attrs);
-  }
-  else {
-    res.status(200).end();
-  }
-});
+// app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// app.use(function (req, res) {
+//   // these are the interested attributes
+//   var attrs = {
+//     headers: req.headers,
+//     hostname: req.hostname,
+//     originalUrl: req.originalUrl,
+//     path: req.path,
+//     query: req.query,
+//     body: req.body,
+//   };
+//   log.info(attrs);
+//   if (argv.body) {
+//     res.status(200).json(attrs);
+//   }
+//   else {
+//     res.status(200).end();
+//   }
+// });
 
-var server = app.listen(argv.port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(argv.port, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('listening at http://%s:%s', host, port);
-});
+//   console.log('listening at http://%s:%s', host, port);
+// });
