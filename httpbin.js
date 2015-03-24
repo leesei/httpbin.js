994be9361beb3460c1d516c89e5f249490c117cf
leesei <leesei@gmail.com>
2015-03-24 11:07:07 +0800

    remove obsolete code
     Changes to be committed:
      modified:   httpbin.js

--- httpbin.js
+++ httpbin.js
@@ -57,36 +57,6 @@ server.route({
   }
 });
 
-
 server.start(function() {
   console.log('Server running at:', server.info.uri);
 });
-
-// http://expressjs.com/4x/api.html#request
-// app.use(bodyParser.json()); // for parsing application/json
-// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-// app.use(function (req, res) {
-//   // these are the interested attributes
-//   var attrs = {
-//     headers: req.headers,
-//     hostname: req.hostname,
-//     originalUrl: req.originalUrl,
-//     path: req.path,
-//     query: req.query,
-//     body: req.body,
-//   };
-//   log.info(attrs);
-//   if (argv.body) {
-//     res.status(200).json(attrs);
-//   }
-//   else {
-//     res.status(200).end();
-//   }
-// });
-
-// var server = app.listen(argv.port, function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
-
-//   console.log('listening at http://%s:%s', host, port);
-// });
